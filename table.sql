b9e2b8482c6bbaf6d067d6ffedd78a21c1c80f4b
milirope <64318749+milirope@users.noreply.github.com>
2021-07-02 23:28:23 -0400

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE plant(
+    type TEXT NOT NULL,
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    flower  TEXT NOT NULL,
+    seed TEXT NOT NULL,
+    fruit TEXT NOT NULL,
+    spore TEXT NOT NULL,
+id_plant SERIAL NOT NULL
+
+);
